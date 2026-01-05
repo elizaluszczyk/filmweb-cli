a270581ca14cb4d27d297a5211dbd2597f539a13
eliza <eliza.luszczyk@gmail.com>
2026-01-05 18:20:49 +0100

    feat: add search command

--- src/filmweb_cli/cli.py
+++ src/filmweb_cli/cli.py
@@ -1,9 +1,26 @@
 import click
 
+from .client import FilmwebClient
+from .services.search_service import SearchService
 
-@click.command()
-def main() -> None:
-    click.echo("Hello from filmweb-cli!")
+
+@click.group()
+@click.pass_context
+def main(ctx: click.Context) -> None:
+    client = FilmwebClient()
+    ctx.obj = client
+
+
+@main.command("search")
+@click.argument("query")
+@click.pass_obj
+def search(client: FilmwebClient, query: str) -> None:
+    search_service = SearchService(client)
+
+    search_results = search_service.search(query)
+
+    for result in search_results.search_hits:
+        click.echo(result)
 
 
 if __name__ == "__main__":
