16cc2958a14102e6a22ed2022545048ce14befac
eliza <eliza.luszczyk@gmail.com>
2026-01-08 00:19:36 +0100

    feat: create specific methods for different types of searches

--- src/filmweb_cli/services/search_service.py
+++ src/filmweb_cli/services/search_service.py
@@ -1,5 +1,11 @@
 from filmweb_cli.client import FilmwebClient
-from filmweb_cli.schemas.search import SearchFilmResponse, SearchSeriesResponse
+from filmweb_cli.schemas.search import (
+    SearchCharacterResponse,
+    SearchFilmResponse,
+    SearchGameResponse,
+    SearchPersonResponse,
+    SearchSeriesResponse,
+)
 
 
 class SearchService:
@@ -13,3 +19,15 @@ class SearchService:
     def search_series(self, query: str) -> SearchSeriesResponse:
         search_series_response = self.client.get("/serials/search", params={"query": query})
         return SearchSeriesResponse.model_validate(search_series_response.json())
+
+    def search_character(self, query: str) -> SearchCharacterResponse:
+        search_character_response = self.client.get("/characters/search", params={"query": query})
+        return SearchCharacterResponse.model_validate(search_character_response.json())
+
+    def search_person(self, query: str) -> SearchPersonResponse:
+        search_person_response = self.client.get("/persons/search", params={"query": query})
+        return SearchPersonResponse.model_validate(search_person_response.json())
+
+    def search_game(self, query: str) -> SearchGameResponse:
+        search_game_response = self.client.get("/games/search", params={"query": query})
+        return SearchGameResponse.model_validate(search_game_response.json())
