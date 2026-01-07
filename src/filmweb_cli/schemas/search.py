9fb2911f86fb79e24cacafb6120396638c4b862b
eliza <eliza.luszczyk@gmail.com>
2026-01-08 00:18:47 +0100

    feat: create models for games, characters and movie people

--- src/filmweb_cli/schemas/search.py
+++ src/filmweb_cli/schemas/search.py
@@ -20,11 +20,38 @@ class SearchSeriesHit(SearchHit):
     type: Literal["serial"]
 
 
-class SearchFilmResponse(BaseModel):
+class SearchGameHit(SearchHit):
+    type: Literal["game"]
+
+
+class SearchCharacterHit(SearchHit):
+    type: Literal["character"]
+    matched_name: str = Field(alias="matchedName")
+
+
+class SearchPersonHit(SearchHit):
+    type: Literal["person"]
+
+
+class SearchResponse(BaseModel):
     total: int
+
+
+class SearchFilmResponse(SearchResponse):
     search_hits: list[SearchFilmHit] = Field(alias="searchHits")
 
 
-class SearchSeriesResponse(BaseModel):
-    total: int
+class SearchSeriesResponse(SearchResponse):
     search_hits: list[SearchSeriesHit] = Field(alias="searchHits")
+
+
+class SearchCharacterResponse(SearchResponse):
+    search_hits: list[SearchCharacterHit] = Field(alias="searchHits")
+
+
+class SearchPersonResponse(SearchResponse):
+    search_hits: list[SearchPersonHit] = Field(alias="searchHits")
+
+
+class SearchGameResponse(SearchResponse):
+    search_hits: list[SearchGameHit] = Field(alias="searchHits")
