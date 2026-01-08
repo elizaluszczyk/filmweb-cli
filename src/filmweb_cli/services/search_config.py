90e505a259ba79bc2b605097d462d0eb2c32c548
eliza <eliza.luszczyk@gmail.com>
2026-01-08 01:27:32 +0100

    feat: add config with search endpoints

---
+++ src/filmweb_cli/services/search_config.py
@@ -0,0 +1,16 @@
+from filmweb_cli.schemas.search import (
+    SearchCharacterHit,
+    SearchFilmHit,
+    SearchGameHit,
+    SearchHit,
+    SearchPersonHit,
+    SearchSeriesHit,
+)
+
+SEARCH_ENDPOINTS: dict[SearchHit, str] = {
+    SearchFilmHit: "/films/search",
+    SearchSeriesHit: "/serials/search",
+    SearchCharacterHit: "/characters/search",
+    SearchPersonHit: "/persons/search",
+    SearchGameHit: "/games/search",
+}
