d49a6c546be2a3dca845376dcb87d11420f7404a
eliza <eliza.luszczyk@gmail.com>
2026-01-08 01:25:43 +0100

    feat: create general method for searching different categories

--- src/filmweb_cli/services/search_service.py
+++ src/filmweb_cli/services/search_service.py
@@ -1,33 +1,17 @@
+from typing import TypeVar
+
 from filmweb_cli.client import FilmwebClient
-from filmweb_cli.schemas.search import (
-    SearchCharacterResponse,
-    SearchFilmResponse,
-    SearchGameResponse,
-    SearchPersonResponse,
-    SearchSeriesResponse,
-)
+from filmweb_cli.schemas.search import SearchHit, SearchResponse
+
+from .search_config import SEARCH_ENDPOINTS
+
+T = TypeVar("T", bound=SearchHit)
 
 
 class SearchService:
     def __init__(self, client: FilmwebClient) -> None:
         self.client = client
 
-    def search_film(self, query: str) -> SearchFilmResponse:
-        search_film_response = self.client.get("/films/search", params={"query": query})
-        return SearchFilmResponse.model_validate(search_film_response.json())
-
-    def search_series(self, query: str) -> SearchSeriesResponse:
-        search_series_response = self.client.get("/serials/search", params={"query": query})
-        return SearchSeriesResponse.model_validate(search_series_response.json())
-
-    def search_character(self, query: str) -> SearchCharacterResponse:
-        search_character_response = self.client.get("/characters/search", params={"query": query})
-        return SearchCharacterResponse.model_validate(search_character_response.json())
-
-    def search_person(self, query: str) -> SearchPersonResponse:
-        search_person_response = self.client.get("/persons/search", params={"query": query})
-        return SearchPersonResponse.model_validate(search_person_response.json())
-
-    def search_game(self, query: str) -> SearchGameResponse:
-        search_game_response = self.client.get("/games/search", params={"query": query})
-        return SearchGameResponse.model_validate(search_game_response.json())
+    def search(self, query: str, hit_type: T) -> SearchResponse[T]:
+        search_response = self.client.get(SEARCH_ENDPOINTS.get(hit_type), params={"query": query})
+        return SearchResponse[hit_type].model_validate(search_response.json())
