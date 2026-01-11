922150e8c357cc7fc30aa653b214c21942d8cd1f
eliza <eliza.luszczyk@gmail.com>
2026-01-11 13:30:22 +0100

    feat: show content preview

---
+++ src/filmweb_cli/services/info_service.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from pydantic import Field, TypeAdapter
+
+from filmweb_cli.client import FilmwebClient
+from filmweb_cli.schemas.info.info import FilmInfo, GameInfo, SeriesInfo
+
+ContentPreview = Annotated[FilmInfo | SeriesInfo | GameInfo, Field(discriminator="entity_name")]
+
+
+class InfoService:
+    def __init__(self, client: FilmwebClient) -> None:
+        self.client = client
+        self.adapter = TypeAdapter(ContentPreview)
+
+    def show_content_preview(self, content_id: int) -> ContentPreview:
+        info_response = self.client.get(f"/film/{content_id}/preview")
+        return self.adapter.validate_python(info_response.json())
