05f83a151f6a6752ef0892fc42fe505794a0a81a
eliza <eliza.luszczyk@gmail.com>
2026-01-12 13:45:20 +0100

    feat: add raw option to use it in fzf

--- src/filmweb_cli/cli.py
+++ src/filmweb_cli/cli.py
@@ -20,22 +20,33 @@ def main(ctx: click.Context) -> None:
 
 @main.command("search")
 @click.argument("query")
+@click.option("--raw", is_flag=True, hidden=True, help="Output raw format")
 @click.pass_obj
-def search(client: FilmwebClient, query: str) -> None:
+def search(client: FilmwebClient, query: str, *, raw: bool) -> None:
     search_service = SearchService(client)
-
     search_results = asyncio.run(search_service.search(query))
 
-    categories: list[tuple[str, Sequence[Displayable]]] = [
-        ("FILMS", search_results.get_films()),
-        ("SERIES", search_results.get_series()),
-        ("GAMES", search_results.get_games()),
-        ("CHARACTERS", search_results.get_characters()),
-        ("PEOPLE", search_results.get_movie_people()),
-        ("WORLDS", search_results.get_worlds()),
-    ]
-
-    print_search_results(categories)
+    if raw:
+        for category, items in [
+            ("FILM", search_results.get_films()),
+            ("SERIES", search_results.get_series()),
+            ("GAME", search_results.get_games()),
+            ("CHARACTER", search_results.get_characters()),
+            ("PERSON", search_results.get_movie_people()),
+            ("WORLD", search_results.get_worlds()),
+        ]:
+            for item in items:
+                click.echo(f"{item.get_id()}\t[{category}] {item.display_name()}")
+    else:
+        categories: list[tuple[str, Sequence[Displayable]]] = [
+            ("FILMS", search_results.get_films()),
+            ("SERIES", search_results.get_series()),
+            ("GAMES", search_results.get_games()),
+            ("CHARACTERS", search_results.get_characters()),
+            ("PEOPLE", search_results.get_movie_people()),
+            ("WORLDS", search_results.get_worlds()),
+        ]
+        print_search_results(categories)
 
 
 @main.command("info")
@@ -43,7 +54,6 @@ def search(client: FilmwebClient, query: str) -> None:
 @click.pass_obj
 def show_info(client: FilmwebClient, content_id: str) -> None:
     info_service = InfoService(client)
-
     content_info = asyncio.run(info_service.show_content_preview(int(content_id)))
 
     print_preview(content_info)
