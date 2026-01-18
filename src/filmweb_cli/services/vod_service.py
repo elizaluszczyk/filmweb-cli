57f45da653869bff7ae63be3e36a1456c9bed10c
eliza <eliza.luszczyk@gmail.com>
2026-01-18 13:48:22 +0100

    feat: create vod service which returns vod providers list

---
+++ src/filmweb_cli/services/vod_service.py
@@ -0,0 +1,20 @@
+from pydantic import TypeAdapter
+
+from filmweb_cli.client import FilmwebClient
+from filmweb_cli.schemas.vod.vod_providers import ContentVodProvider, VodProvider
+
+VOD_ADAPTER = TypeAdapter(list[VodProvider])
+CONTENT_VOD_ADAPTER = TypeAdapter(list[ContentVodProvider])
+
+
+class VodService:
+    def __init__(self, client: FilmwebClient) -> None:
+        self.client = client
+
+    async def get_vod_providers(self) -> list[VodProvider]:
+        vod_response = await self.client.get("/vod/providers/list")
+        return VOD_ADAPTER.validate_python(vod_response.json())
+
+    async def get_content_vod_providers(self, content_id: int) -> list[ContentVodProvider]:
+        content_vod_response = await self.client.get(f"/vod/film/{content_id}/providers/list")
+        return CONTENT_VOD_ADAPTER.validate_python(content_vod_response.json())
