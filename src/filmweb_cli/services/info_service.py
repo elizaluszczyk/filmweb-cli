ab79420504224e8a16df795458c4cb3b144f1aea
eliza <eliza.luszczyk@gmail.com>
2026-01-19 13:54:51 +0100

    feat: add method to return person preview and fetch titles for which person is known for

--- src/filmweb_cli/services/info_service.py
+++ src/filmweb_cli/services/info_service.py
@@ -1,9 +1,11 @@
+import asyncio
 from typing import Annotated
 
 from pydantic import Field, TypeAdapter
 
 from filmweb_cli.client import FilmwebClient
-from filmweb_cli.schemas.info.info import FilmInfo, FullDescription, GameInfo, SeriesInfo
+from filmweb_cli.schemas.info.content_info import FilmInfo, FullDescription, GameInfo, SeriesInfo
+from filmweb_cli.schemas.info.people_characters_info import PersonInfo
 from filmweb_cli.schemas.info.rating import ContentRating, Rating
 
 ContentPreview = Annotated[FilmInfo | SeriesInfo | GameInfo, Field(discriminator="entity_name")]
@@ -37,3 +39,27 @@ class InfoService:
     async def get_full_description(self, content_id: int) -> FullDescription:
         description_response = await self.client.get(f"/film/{content_id}/description")
         return FullDescription.model_validate(description_response.json())
+
+    async def get_person_preview(self, person_id: int) -> PersonInfo:
+        person_raw_response = await self.client.get(f"/person/{person_id}/preview")
+        response = PersonInfo.model_validate(person_raw_response.json())
+
+        await self._fetch_person_content(response)
+
+        return response
+
+    async def _fetch_person_content(self, response: PersonInfo) -> None:
+        tasks = [self.get_content_preview(film_id) for film_id in response.films_known_for]
+
+        if not tasks:
+            return
+
+        details = await asyncio.gather(*tasks)
+
+        titles: list[str] = []
+
+        for item in details:
+            title = item.title.title if item.title else item.original_title.title
+            titles.append(title)
+
+        response.known_for_titles = titles
